1f05c3a5e007f96e463d747c831798b6642b56e2
tjorge-l <tjorge-l@student.42lisboa.com>
2024-11-11 10:07:37 +0000

    Add file scaffold

---
+++ philo/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjorge-l < tjorge-l@student.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 10:00:09 by tjorge-l          #+#    #+#             */
+/*   Updated: 2024/11/11 10:02:40 by tjorge-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h>
+#endif
