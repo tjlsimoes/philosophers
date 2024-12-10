33c2fe395cb597e39ecea5126743ec641175c449
tjorge-l <tjorge-l@student.42lisboa.com>
2024-12-10 10:09:25 +0000

    Organize header file

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 10:00:09 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/12/04 14:37:34 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/12/10 10:07:50 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,53 +61,56 @@ typedef struct s_env
 	pthread_mutex_t	state_mutex;
 }	t_env;
 
-// Initial Error Check
-
+// Main auxiliary functions
 void			check_args(int argc, char **argv);
+void			philosophers_init(t_env **env);
+void			forks_init(t_env **env);
+t_env			*env_init(int argc, char **argv);
+
+// Initial Error Check
 void			error(char *error_msg);
 void			phil_lst_error(char *error_msg,
 					t_env **env, unsigned int idx);
 void			fork_lst_error(char *error_msg,
 					t_env **env, unsigned int idx);
-int				ft_isspace(char c);
 int				ft_strlen(char *str);
-int				is_posnbr(char *nptr);
 
 // Initializer auxiliary functions
-
-unsigned int	atui(char *nptr);
 void			*ft_calloc(size_t nmemb, size_t size);
-
-void			philosophers_init(t_env **env);
-void			phil_lstadd_back(t_env **env, t_phil *new, unsigned int i);
-void			phil_lstclear(t_env **env, unsigned int lst_size);
-void			phil_last_link(t_env **env);
+int				ft_isspace(char c);
+int				is_posnbr(char *nptr);
+unsigned int	atui(char *nptr);
 void			phil_lst_check(t_env **env, unsigned int lst_size);
 
-void			forks_init(t_env **env);
+void			phil_lstclear(t_env **env, unsigned int lst_size);
+void			phil_lstadd_back(t_env **env, t_phil *new, unsigned int i);
+void			phil_last_link(t_env **env);
+void			create_threads(t_env **env, unsigned int lst_size);
+void			join_threads(t_env **env, unsigned int lst_size);
+
 void			fork_lstclear(t_env **env, unsigned int lst_size);
 void			fork_lstadd_back(t_env **env, t_fork *new, unsigned int i);
 void			fork_lst_check(t_env **env, unsigned int lst_size);
 t_fork			*get_fork_nbr(t_env **env, unsigned int nbr);
 
-void			create_threads(t_env **env, unsigned int lst_size);
-void			join_threads(t_env **env, unsigned int lst_size);
-
-int				dead_check(long current, long last, long die_time);
-long			get_time(void);
+// Routine functions
 void			eat(t_phil **phil);
 void			rest(t_phil **phil);
 void			think(t_phil **phil);
+int				action(void (*f)(t_phil **), t_phil **phil);
 void			*routine(void *arg);
 
-int				action(void (*f)(t_phil **), t_phil **phil);
 int				end_check(t_phil **phil);
+long			get_time(void);
+int				dead_check(long current, long last, long die_time);
+
 void			smart_sleep(t_phil **phil, long time);
 void			eat_smart_sleep(t_phil **phil, long time);
 void			msg_write(t_phil **phil, char *str);
 int				full_check(t_phil **phil);
-int				one_phil_check(t_phil **phil);
 int				general_full_check(t_phil **phil, int i);
+
+int				one_phil_check(t_phil **phil);
 void			update_env_full(t_phil **phil);
 void			update_dead(t_phil **phil);
 
